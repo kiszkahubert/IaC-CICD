e19a24c96da03dc8d594e22897a47b9e14bea28b
Hubert <kiszkahub@gmail.com>
2025-08-12 16:20:07 +0200

    ???

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:18-alpine
 WORKDIR /app
 COPY application/package.json application/package-lock.json* ./
-RUN npm ci && npm audit fix --force
+RUN npm ci && npm install cross-spawn@7.0.5 --save-exact && npm dedupe
 COPY application/ .
 EXPOSE 3000
 CMD ["npm", "start"]
--- application/server.js
+++ application/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const nodeName = process.env.NODE_NAME||'unknown-node';
+const nodeName = process.env.NODE_NAME || 'unknown-node';
 
 app.get('/', (req, res) => {
   res.send(`Hello from Kubernetes pod running on node: ${nodeName} - time: ${new Date().toISOString()}`);
